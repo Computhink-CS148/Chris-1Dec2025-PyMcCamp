c20d58794a6e2750821ef60daa9063508806820f
Computhink-CS148 <cs148@computhink.com.sg>
2025-12-01 08:51:50 +0000

    dAY1

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,38 +4,81 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
+print("Hello World")
 
 
 
 ########################################################################
 # Task 2:
+print("Christopher/Chris for short")
+print("11 years old")
+print("Seng Kang Primary school")
+print("I would spend it on a new gaming console and more games!")
 
 
 
-########################################################################
+# 
 # Task 3:
+Animal1="Tiger"
+Animal2="Lion"
+Animal3="Sheep"
+print(Animal1,Animal2,Animal3)
 
 
 
-########################################################################
+
+# ########################################################################
 # Task 4:
-
-
+var1=5
+var2=10
+print(var1+var2)
+print(var1*var2)
+print(var1/var2)
+print(var1-var2)
 
 ########################################################################
 # Task 5:
+name = "Chris"
+food = "Chicken Rice"
+print("My name is " + name + ".")
+print("I like to eat " + food + ".")
 
 
 
 ########################################################################
 # Task 6:
+def intro():
+    print("Christopher/Chris for short")
+    print("11 years old")
+    print("Seng Kang Primary school")
+    print("I would spend it on a new gaming console and more games!")
+intro()
 
 
 
 ########################################################################
 # Task 7:
+def addition(num1, num2):
+    print(num1+num2)
+addition(321747,1846)
 
 
 
-########################################################################
 # Additional exercises:
+# print("Hey")
+# print("Bye")
+# print("This is really simple but important")
+# print("Python reads from the top to the bottom")
+# #Exercise 2
+# greet = "hello"
+# print(greet)
+# #exercise 3
+
+# title = "Emperor"
+# name = "Johnson"
+# command = "eat cake"
+# #exercise 4
+# def hello():
+#     print("Hello, how are you?")
+#     print("I am Chris")
+# hello()
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,46 @@
 
 
 ################## On Chat Commands Section #####################
+def move_forward(steps):
+    agent.move(FORWARD, steps)
+def move_back(steps):
+    agent.move(BACK, steps)
+def move_up(steps):
+    agent.move(UP, steps)
+
+def move_down(steps):
+    agent.move(DOWN, steps)
+
+def turn_left():
+    agent.turn(TurnDirection.LEFT)
+
+def turn_right():
+    agent.turn(TurnDirection.RIGHT)
+
+player.on_chat("mf1",move_forward)
+player.on_chat("mb1",move_back)
+player.on_chat("mu1",move_up)
+player.on_chat("md1",move_down)
+player.on_chat("tl",turn_left)
+player.on_chat("tr",turn_right)
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come",teleport)
+
+def obby1():
+    agent.move(FORWARD, 4)
+    agent.move(LEFT, 4)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 2)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+player.on_chat("obby",obby1)
