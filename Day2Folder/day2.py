2662aed9cd6d603a76a7fd3928852066a42b1a44
Computhink-CS148 <cs148@computhink.com.sg>
2025-12-02 06:15:24 +0000

    Dec 2, 2025, 6:15 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -4,7 +4,7 @@ print("hello from day2")
 
 ########################################################################
 # Task 1:
-for count in range(100):
+for count in range(00):
     print("Hello")
 
 
