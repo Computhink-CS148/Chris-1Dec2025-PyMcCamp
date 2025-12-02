7dbf56a4006d638d16a9e187144f0eb459c24273
Computhink-CS148 <cs148@computhink.com.sg>
2025-12-02 08:48:12 +0000

    Dec 2, 2025, 8:48 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -4,35 +4,66 @@ print("hello from day2")
 
 ########################################################################
 # Task 1:
-for count in range(100):
-    print("Hello")
+# for count in range(100):
+#     print("Hello")
 
 
 
 ########################################################################
 # Task 2:
+# def cheer(name):
+#     for letter in name:
+#         print("Give me a " + letter + "!")
+#     print("Who is the best??!?")
+#     print(name + "!")
+# cheer("Adam".upper())
 
 
 
 ########################################################################
 # Task 3:
-
+# for counter in range(10):
+#     print(counter)
 
 
 ########################################################################
 # Task 4:
+# for count in range(1,11):
+#     print(count)
 
 
 
-########################################################################
-# Task 5:
+# ########################################################################
+# # Task 5:
+# for count in range(5,33):
+#     print(count)
 
 
+# ########################################################################
+# # Task 6:
+# for count in range(50,0,-1):
+#     print(count)
 
-########################################################################
-# Task 6:
 
-
-
-########################################################################
+###########################################################################
 # Additional exercises:
+for count in range(67):
+    print(count)
+
+for count in range(11):
+    print(count)
+
+for count in range(7,33):
+    print(count)
+
+for count in range(65,100):
+    print(count)
+
+for count in range(2,33,2):
+    print(count)
+
+for count in range(5,101,5):
+    print(count)
+
+for count in range(100,-1,-2):
+    print(count)
--- Day2Folder/minecraft_day2.py
+++ Day2Folder/minecraft_day2.py
@@ -1,2 +1,87 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def move_forward(steps):
+    agent.move(FORWARD, steps)
+def move_back(steps):
+    agent.move(BACK, steps)
+def move_up(steps):
+    agent.move(UP, steps)
+
+def move_down(steps):
+    agent.move(DOWN, steps)
+
+def turn_left():
+    agent.turn(TurnDirection.LEFT)
+
+def turn_right():
+    agent.turn(TurnDirection.RIGHT)
+
+player.on_chat("mf1",move_forward)
+player.on_chat("mb1",move_back)
+player.on_chat("mu1",move_up)
+player.on_chat("md1",move_down)
+player.on_chat("tl",turn_left)
+player.on_chat("tr",turn_right)
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come",teleport)
+
+def obby1():
+    agent.move(FORWARD, 4)
+    agent.move(LEFT, 4)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 2)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+player.on_chat("obby",obby1)
+
+def chop(height):
+    agent.destroy(FORWARD)
+    for i in range(height-1):
+        agent.destroy(UP)
+        agent.move(UP, 1)
+    agent.move(DOWN,height-1)
+    agent.collect_all()
+player.on_chat("chop",chop)
+
+def new_chop():
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.turn(TurnDirection.LEFT)
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.turn(TurnDirection.LEFT)
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.turn(TurnDirection.LEFT)
+    agent.destroy(FORWARD)
+    agent.turn(TurnDirection.RIGHT)
+    agent.destroy(FORWARD)
+    agent.turn(TurnDirection.RIGHT)
+    agent.turn(TurnDirection.RIGHT)
+    agent.move(UP, 1)
+
+player.on_chat("new_chop",new_chop)
+
+def mine_ore(length):
+    for e in range(length):
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(FORWARD)
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD,1)
+
+player.on_chat("mineore",mine_ore)
+
