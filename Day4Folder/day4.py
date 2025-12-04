6cb710bdce0c1a34a6d794b6c01c555d0718781e
Computhink-CS148 <cs148@computhink.com.sg>
2025-12-04 08:10:39 +0000

    Dec 4, 2025, 8:10 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -1,16 +1,31 @@
-# Write all your codes for Day 4 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day4")
+# # Write all your codes for Day 4 here.
+# # COMMENT out the previous task before going on to the next task
+# # print("hello from day4")
 
-########################################################################
-# Task 1:
+# ########################################################################
+# # Task 1:
+# count = 0
+# while count < 10:
+#     print(count)
+#     count = count+1
+# print("____________________________")
 
+# count = 5
+# while count < 33:
+#     print(count)
+#     count = count+1
+# print("____________________________")
 
+# count = 50
+# while count > 0:
+#     print(count)
+#     count = count-1
 
 ########################################################################
 # Task 2:
-
-
-
+# ans = input("What can you catch but not throw? ")
+# while ans != "A cold":
+#     ans = input("What can you catch but not throw? ")
+# print("You got it correct!")
 ########################################################################
 # Additional exercises:
