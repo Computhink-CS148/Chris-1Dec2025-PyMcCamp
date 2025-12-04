7dc3dd2da571301d1a5be2c306f3cc6a810f85c7
Computhink-CS148 <cs148@computhink.com.sg>
2025-12-04 08:56:04 +0000

    Dec 4, 2025, 8:56 AM

--- Day4Folder/minecraft_day4.py
+++ Day4Folder/minecraft_day4.py
@@ -1,2 +1,125 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def move_forward(steps):
+    agent.move(FORWARD, steps)
+def move_back(steps):
+    agent.move(BACK, steps)
+def move_up(steps):
+    agent.move(UP, steps)
+
+def move_down(steps):
+    agent.move(DOWN, steps)
+
+def turn_left():
+    agent.turn(TurnDirection.LEFT)
+
+def turn_right():
+    agent.turn(TurnDirection.RIGHT)
+
+player.on_chat("mf1",move_forward)
+player.on_chat("mb1",move_back)
+player.on_chat("mu1",move_up)
+player.on_chat("md1",move_down)
+player.on_chat("tl",turn_left)
+player.on_chat("tr",turn_right)
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come",teleport)
+
+def obby1():
+    agent.move(FORWARD, 4)
+    agent.move(LEFT, 4)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 2)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+player.on_chat("obby",obby1)
+
+def chop(height):
+    agent.destroy(FORWARD)
+    for i in range(height-1):
+        agent.destroy(UP)
+        agent.move(UP, 1)
+    agent.move(DOWN,height-1)
+    agent.collect_all()
+player.on_chat("chop",chop)
+
+def new_chop():
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.turn(TurnDirection.LEFT)
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.turn(TurnDirection.LEFT)
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.turn(TurnDirection.LEFT)
+    agent.destroy(FORWARD)
+    agent.turn(TurnDirection.RIGHT)
+    agent.destroy(FORWARD)
+    agent.turn(TurnDirection.RIGHT)
+    agent.turn(TurnDirection.RIGHT)
+    agent.move(UP, 1)
+
+player.on_chat("new_chop",new_chop)
+
+def mine_ore(length):
+    for e in range(length):
+        agent.destroy(DOWN)
+        agent.destroy(FORWARD)
+        agent.collect_all()
+        agent.move(FORWARD,1)
+
+player.on_chat("mineore",mine_ore)
+
+def build_wall(length,width):
+    for b in range(width):
+        for a in range(length):
+            agent.place(FORWARD)
+            agent.move(UP, 1)
+        agent.move(DOWN,length+1)
+        agent.move(BACK, 1)
+player.on_chat("buil",build_wall)
+
+def build_roof(length):
+    for a in range(length):
+        agent.place(FORWARD)
+        agent.move(BACK,1)
+player.on_chat("build",build_roof)
+
+
+
+def spiral():
+    while agent.inspect_block(DOWN) != GOLD_BLOCK:
+        if not agent.detect(AgentDetection.BLOCK, RIGHT):
+            agent.turn(TurnDirection.RIGHT)
+        agent.move(FORWARD,1)
+
+player.on_chat("ss",spiral)      
+def maze():
+    while agent.inspect_block(DOWN) != DIAMOND_BLOCK:
+        if not agent.detect(AgentDetection.BLOCK, LEFT):
+            agent.turn(TurnDirection.LEFT) 
+        else:
+            if agent.detect(AgentDetection.BLOCK, FORWARD):
+                agent.turn(TurnDirection.RIGHT)
+        agent.move(FORWARD,1)
+player.on_chat("maze",maze)
+
+
+def dig(depth):
+    for a in range(depth):
+        agent.destroy(DOWN)
+        agent.move(DOWN, 1)
+player.on_chat("dig",dig)
